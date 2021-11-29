96274340f9a5c02b9793840fc233591faec5c08b
umnovjp <umnovjp@yahoo.com>
2021-11-28 21:39:08 -0600

    most routes work. but put route does not work

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,20 +6,21 @@ VALUES (1, 'Scientology'),
        (5, 'Zoroastrianism');
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Sr Scientologist', 100000, 6),
-       (2, 'Principal Ritualist', 25000, 7),
-       (3, 'Strategic Jedi', 1000000, 8),
-       (4, 'Lead Prophet', 1, 9),
-       (5, 'Zoroaster', 5, 10),
-       (6, 'Apprentice', 30000, 11),
-       (7, 'Junior Alabamacare Adviser', 25000, 12);
+VALUES (1, 'Sr Scientologist', 100000, 2),
+       (2, 'Principal Ritualist', 25000, 5),
+       (3, 'Strategic Jedi', 1000000, 4),
+       (4, 'Lead Prophet', 1, 2),
+       (5, 'Zoroaster', 5, 1),
+       (6, 'Apprentice', 30000, 1),
+       (7, 'Former first priestess', 30000000, 3),
+       (8, 'Junior Alabamacare Adviser', 25000, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Jo3', 'Bid3n', 13, 1),
-       (2, 'Ronald', 'Tramp', 666, 2),
-       (3, 'Tony', 'Blinking', 1984, 3),
-       (4, 'Clint', 'Billton', 1776, 4),
-       (5, 'Jonny', 'Lynchy', 2001, 5),
-       (6, 'Mallory', 'Billton', 2020, 6),
-       (7, 'Baruch', 'Alabama', 2012, 7);
+VALUES (1, 'Jo3', 'Bid3n', 6, 1),
+       (2, 'Ronald', 'Tramp', 1, 2),
+       (3, 'Tony', 'Blinking', 2, 3),
+       (4, 'Clint', 'Billton', 7, 4),
+       (5, 'Jonny', 'Lynchy', 3, 5),
+       (6, 'Mallory', 'Billton', 4, 6),
+       (7, 'Baruch', 'Alabama', 8, 7);
 
--- server.js
+++ server.js
@@ -19,11 +19,118 @@ const db = mysql.createConnection(
     password: 'Virahovs74#',
     database: 'employee_db'
   },
-  console.log(`Connected to the classlist_db database.`)
+  console.log(`Connected to the employee_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM students', function (err, results) {
+// Read department id and name: first get route
+app.get('/db/department', (req, res) => {
+    const sql = `SELECT id, name AS name FROM department`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  // Read roles: second get route
+app.get('/db/role', (req, res) => {
+    const sql = `SELECT * FROM role`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+    // Read employees: third get route
+  app.get('/db/employee', (req, res) => {
+    const sql = `SELECT id, first_name AS first_name FROM employee`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// Add a department first post route
+app.post('/db/department', ({ body }, res) => {
+    const sql = `INSERT INTO department (name)
+      VALUES (?)`;
+    const params = [body.name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+  //second post route to add role
+  app.post('/db/role', ({ body }, res) => {
+    const sql = `INSERT INTO role (title)
+      VALUES (?)`;
+    const params = [body.title];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+  // Update role
+app.put('/db/role/:id', (req, res) => {
+    const sql = `UPDATE role SET title = ? WHERE id = ?`;
+    const params = [req.body.department_id, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'role not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+  
+
+// Query database puts department table to console.log
+db.query('SELECT * FROM department', function (err, results) {
   console.log(results);
 });
 
